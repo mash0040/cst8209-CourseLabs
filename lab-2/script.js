cd9d7dcba8398da250def575eda4fbafe863ef2c
mash0040 <mash0040@algonquinlive.com>
2023-09-15 20:32:33 -0400

    Completes Lab 2

--- lab-2/script.js
+++ lab-2/script.js
@@ -2,5 +2,5 @@ let firstName = "Ekene";
 console.log ("My name is " + firstName);
 
 let favNumber = 10;
-let squared = 100;
+let squared = favNumber * favNumber;
 console.log (`The squared of`, favNumber, `is` , squared);
